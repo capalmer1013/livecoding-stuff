40ef83d9ceb2c340204621f2ecb302aa9bb17ddb
Chris Palmer <capalmer1013@gmail.com>
2023-03-09 11:38:42 -0500

    some changes

--- sonic-pi/test-sonicpi.rb
+++ sonic-pi/test-sonicpi.rb
@@ -1,8 +1,13 @@
 use_random_seed Time.now.to_f
 use_bpm 80
 use_synth :sine
-
 moto = "/home/cpalmer/Music/samples/motorcycle.wav"
+
+root = 108
+wave_freq = 15
+
+use_tuning :just, hz_to_midi(root)
+
 
 # functions
 # ================================================================================================
@@ -17,14 +22,14 @@ define :create_melody do |len, max_depth=2|
     if max_depth > 0
       case note_type
       when 1
-        note = create_note(pitch=choose(scale(:c2, :major, num_octaves: 2)))
+        note = create_note(pitch=choose(chord(hz_to_midi(root), :major, num_octaves: 3)))
       when 2
         note = create_melody(rrand_i(1, len), max_depth=max_depth-1)
       when 3
         note = :r
       end
     else
-      note = create_note(pitch=choose(scale(:c2, :major, num_octaves: 2)))
+      note = create_note(pitch=choose(chord(hz_to_midi(root), :major, num_octaves: 2)))
     end
     result.push(note)
   end
@@ -39,19 +44,21 @@ define :play_melody do |melody, t|
       play_melody(n, new_t)
       
     elsif n.is_a? Hash
-      play n[:pitch], amp: n[:amp], release: new_t
+      play n[:pitch], amp: n[:amp], release: new_t, attack: n[:attack], pan: n[:pan]
       sleep new_t
       
     end
   end
 end
 
-define :create_note do |pitch=60, amp=0.5, pan=0.0, attack=0.0, decay=0.0, sustain=0.0|
+define :create_note do |pitch=60, amp=0.7, pan=0.0, attack=0.0, decay=0.0, sustain=0.0|
+  attack_values = [0.125, 0.25, 0.5, 1]
   note = {
     :pitch => pitch,
-    :amp => amp,
-    :pan => pan,
+    :amp => rrand(0.2, 0.8),
+    :pan => rrand(-0.8, 0.8),
     :synth => current_synth,
+    :attack => choose(attack_values),
   }
   return note
 end
@@ -70,12 +77,13 @@ end
 # live_loops
 # ================================================================================================
 live_loop :binaural_beats do
+  amp = 0.05
   with_synth :sine do
-    play 60, release: 8, decay: 0, sustain: 8, pan: -1, amp: 0.2, attack: 1.0
-    play 60.5, release: 8, decay: 0, sustain: 8, pan: 1, amp: 0.2, attack: 1.0
+    play hz_to_midi(root),           release: 8, decay: 0, sustain: 7, pan: -1, amp: amp, attack: 1.0
+    play hz_to_midi(root+wave_freq), release: 8, decay: 0, sustain: 7, pan: 1,  amp: amp, attack: 1.0
   end
   with_synth :pnoise do
-    play 60.5, release: 8, decay: 0, sustain: 8, amp: 0.2, attack: 1.0
+    play root, release: 8, decay: 0, sustain: 8, amp: amp*0.05, attack: 1.0
   end
   sleep 8
 end
@@ -86,12 +94,12 @@ live_loop :a do
 end
 
 live_loop :bc do
-  stop
-  melody = create_melody(3)
+  melody = create_melody(3, max_depth=1)
   fx_list = [:bitcrusher, :reverb]
   play_fn = lambda { play_melody(melody, 3.0) }
   4.times do
-    play_melody(melody, 3.0)
+    play_melody(melody, 12.0)
   end
   
 end
+puts chord(:c, '11+')

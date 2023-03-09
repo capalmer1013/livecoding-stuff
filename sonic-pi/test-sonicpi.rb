904c4ad5ec6150732213cccb242a317f1b3866df
Chris Palmer <capalmer1013@gmail.com>
2023-03-08 22:16:50 -0500

    more code

--- sonic-pi/test-sonicpi.rb
+++ sonic-pi/test-sonicpi.rb
@@ -4,20 +4,47 @@ use_synth :sine
 
 moto = "/home/cpalmer/Music/samples/motorcycle.wav"
 
-puts current_synth
 # functions
 # ================================================================================================
-define :create_melody do |len|
+define :create_melody do |len, max_depth=2|
   result = []
   len.times do
-    note = create_note(pitch=choose(scale(:c2, :major, num_octaves: 2)), duration=rrand(0.125, 1.0))
+    # 1. note
+    # 2. list
+    # 3. :r
+    note_type = choose([1, 2, 3])
+    case note_type
+    when 1
+      note = create_note(pitch=choose(scale(:c2, :major, num_octaves: 2)), duration=rrand(0.125, 1.0))
+    when 2
+      note = create_melody(len, max_depth=max_depth-1)
+    when 3
+      note = :r
+    else
+      puts "Invalid note type"
+    end
+    
     result.push(note)
   end
   return result
 end
 
-define :modify_melody do |l, n_changes|
+define :play_melody do |melody, t|
+  puts melody
+  new_t = t/melody.length
+  melody.each do |n|
+    if n.is_a?(Array)
+      play_melody(n, new_t)
+    elsif n.is_a? Hash
+      play n[:pitch], amp: n[:amp], release: n[:duration]
+      sleep new_t
+    else
+      puts n
+      puts "invalid melody thing"
+    end
+  end
 end
+
 
 define :create_note do |pitch=60, duration=0.5, amp=0.5, pan=0.0, attack=0.0, decay=0.0, sustain=0.0, release=1.0|
   note = {
@@ -34,31 +61,15 @@ end
 
 # Variables
 # ================================================================================================
-melody = create_melody(16)
-melody2 = create_melody(16)
-sleep_time = 0.125
+melody = create_melody(4)
 
 # live_loops
 # ================================================================================================
 live_loop :a do
-  with_fx :reverb do
-    sample moto, amp: rand(0.5), attack: rand(1.0), rate: rrand(0.1, 0.5), pan: rrand(-1.0, 1.0)
-  end
-  
+  sample moto, amp: rand(0.5), attack: rand(1.0), rate: rrand(0.1, 0.5), pan: rrand(-1.0, 1.0)
   sleep rand(2.0)
 end
 
-
 live_loop :melody do
-  melody.each do |n|
-    play n[:pitch], amp: n[:amp], release: n[:duration]
-    sleep n[:duration]
-  end
-end
-
-live_loop :melody2 do
-  melody2.each do |n|
-    play n[:pitch], amp: n[:amp], release: n[:duration]
-    sleep n[:duration]
-  end
+  play_melody(melody, 4)
 end

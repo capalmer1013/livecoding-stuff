2e81ad21507cfb4ab91ebb9299a124f21badca10
Chris Palmer <capalmer1013@gmail.com>
2023-03-08 22:58:04 -0500

    cleaned up some sonic pi stuff

--- sonic-pi/test-sonicpi.rb
+++ sonic-pi/test-sonicpi.rb
@@ -12,18 +12,20 @@ define :create_melody do |len, max_depth=2|
     # 1. note
     # 2. list
     # 3. :r
-    note_type = choose([1, 2, 3])
-    case note_type
-    when 1
-      note = create_note(pitch=choose(scale(:c2, :major, num_octaves: 2)), duration=rrand(0.125, 1.0))
-    when 2
-      note = create_melody(len, max_depth=max_depth-1)
-    when 3
-      note = :r
+    puts [1, 2] + [2]*max_depth
+    note_type = choose([1, 2] + [2]*max_depth)
+    if max_depth > 0
+      case note_type
+      when 1
+        note = create_note(pitch=choose(scale(:c2, :major, num_octaves: 2)))
+      when 2
+        note = create_melody(rrand_i(1, len), max_depth=max_depth-1)
+      when 3
+        note = :r
+      end
     else
-      puts "Invalid note type"
+      note = create_note(pitch=choose(scale(:c2, :major, num_octaves: 2)))
     end
-    
     result.push(note)
   end
   return result
@@ -35,21 +37,19 @@ define :play_melody do |melody, t|
   melody.each do |n|
     if n.is_a?(Array)
       play_melody(n, new_t)
+      
     elsif n.is_a? Hash
-      play n[:pitch], amp: n[:amp], release: n[:duration]
+      play n[:pitch], amp: n[:amp], release: new_t
       sleep new_t
-    else
-      puts n
-      puts "invalid melody thing"
+      
     end
   end
 end
 
 
-define :create_note do |pitch=60, duration=0.5, amp=0.5, pan=0.0, attack=0.0, decay=0.0, sustain=0.0, release=1.0|
+define :create_note do |pitch=60, amp=0.5, pan=0.0, attack=0.0, decay=0.0, sustain=0.0, release=1.0|
   note = {
     :pitch => pitch,
-    :duration => duration,
     :amp => amp,
     :pan => pan,
     :synth => current_synth,
@@ -58,18 +58,17 @@ define :create_note do |pitch=60, duration=0.5, amp=0.5, pan=0.0, attack=0.0, de
   return note
 end
 
-
-# Variables
-# ================================================================================================
-melody = create_melody(4)
-
 # live_loops
 # ================================================================================================
 live_loop :a do
-  sample moto, amp: rand(0.5), attack: rand(1.0), rate: rrand(0.1, 0.5), pan: rrand(-1.0, 1.0)
+  # sample moto, amp: rand(0.1), attack: rand(1.0), rate: rrand(0.1, 0.5), pan: rrand(-1.0, 1.0)
   sleep rand(2.0)
 end
 
-live_loop :melody do
-  play_melody(melody, 4)
+live_loop :bc do
+  melody = create_melody(3)
+  4.times do
+    play_melody(melody, 3.0)
+  end
+  
 end

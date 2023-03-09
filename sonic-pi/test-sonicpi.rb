0486c91139f84e2a03dd042b06616f637a263304
Chris Palmer <capalmer1013@gmail.com>
2023-03-08 21:41:16 -0500

    adding more stuff

--- sonic-pi/test-sonicpi.rb
+++ sonic-pi/test-sonicpi.rb
@@ -1,14 +1,17 @@
 use_random_seed Time.now.to_f
 use_bpm 80
+use_synth :sine
+
 moto = "/home/cpalmer/Music/samples/motorcycle.wav"
 
-
+puts current_synth
 # functions
 # ================================================================================================
 define :create_melody do |len|
   result = []
   len.times do
-    result.push(choose(scale(:c2, :major, num_octaves: 2)))
+    note = create_note(pitch=choose(scale(:c2, :major, num_octaves: 2)), duration=rrand(0.125, 1.0))
+    result.push(note)
   end
   return result
 end
@@ -16,11 +19,13 @@ end
 define :modify_melody do |l, n_changes|
 end
 
-define :create_notehash do ||
+define :create_note do |pitch=60, duration=0.5, amp=0.5, pan=0.0, attack=0.0, decay=0.0, sustain=0.0, release=1.0|
   note = {
-    :pitch => 60,
-    :duration => 0.5,
-    :amp => 0.5
+    :pitch => pitch,
+    :duration => duration,
+    :amp => amp,
+    :pan => pan,
+    :synth => current_synth,
   }
   # play note[:pitch], sustain: note[:duration], amp: note[:amp]
   return note
@@ -30,13 +35,14 @@ end
 # Variables
 # ================================================================================================
 melody = create_melody(16)
-sleep_time = 0.25
+melody2 = create_melody(16)
+sleep_time = 0.125
 
 # live_loops
 # ================================================================================================
 live_loop :a do
   with_fx :reverb do
-    sample moto, amp: rand(3.0), attack: rand(1.0), rate: rrand(0.1, 0.5), pan: rrand(-1.0, 1.0)
+    sample moto, amp: rand(0.5), attack: rand(1.0), rate: rrand(0.1, 0.5), pan: rrand(-1.0, 1.0)
   end
   
   sleep rand(2.0)
@@ -45,7 +51,14 @@ end
 
 live_loop :melody do
   melody.each do |n|
-    play n, amp: rrand(0.1, 0.3)
-    sleep sleep_time
+    play n[:pitch], amp: n[:amp], release: n[:duration]
+    sleep n[:duration]
+  end
+end
+
+live_loop :melody2 do
+  melody2.each do |n|
+    play n[:pitch], amp: n[:amp], release: n[:duration]
+    sleep n[:duration]
   end
 end

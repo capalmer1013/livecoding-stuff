317e364e762f60cb41195750c4141d69a164aa94
Chris Palmer <capalmer1013@gmail.com>
2023-04-06 00:02:45 -0400

    Create ambient.rb

---
+++ sonic-pi/4-5-2023/ambient.rb
@@ -0,0 +1,7 @@
+root_dir = "D:\\Users\\Chris Palmer\\Documents\\livecoding-stuff\\sonic-pi\\4-5-2023\\Hainbach - Isolation Loops (soundpack)"
+
+
+sample root_dir, 15, rate: -0.5, amp: 1.4, pan: 0.5
+sample root_dir, 15, rate: 0.5, amp: 1.4, pan: -0.5
+
+sample root_dir, 27, rate: 0.2, amp: 2
